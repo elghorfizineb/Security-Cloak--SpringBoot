b42bc7dee6cdfcb6c7b1ca10691566d148bd3270
elghorfizineb <elghorfizineb@gmail.com>
2022-01-15 16:02:34 +0100

    KeyCloack first push

---
+++ products-app-kc-main/src/main/java/ma/enset/productsapp/sec/keycloakAdapterConfig.java
@@ -0,0 +1,16 @@
+package ma.enset.productsapp.sec;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class keycloakAdapterConfig {
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    }
+
---
+++ suppliers-service-kc-main/src/main/java/ma/enset/supplierservice/sec/keycloakAdapterConfig.java
@@ -0,0 +1,16 @@
+package ma.enset.supplierservice.sec;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class keycloakAdapterConfig {
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    }
+
